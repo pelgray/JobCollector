578a860b01671b303aad793c6d73b91a1b0ca85f
pelgray <pelag.bv@gmail.com>
2020-08-11 14:41:31 +0300

    #2 Исправления 3

--- src/main/java/com/pelgray/commands/AddVacancyMobileLink.java
+++ src/main/java/com/pelgray/commands/AddVacancyMobileLink.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Component;
 public class AddVacancyMobileLink extends AddVacancyURL {
     @Override
     protected String regex() {
-        return "^(?:Интересная вакансия) (?:\".+\" — )(https?:\\/\\/hh\\.ru\\/vacancy\\/\\d+\\?from=share_android)" +
+        return "^(?:Интересная вакансия \".+\" — )(https?:\\/\\/hh\\.ru\\/vacancy\\/\\d+\\?from=share_android)" +
                 "(?:\\nОтправлено с помощью мобильного приложения hh https:\\/\\/hh\\.ru\\/mobile\\?from=share_android)$";
     }
 }
