68a581b9bb7d027294411ec830e1dba329348610
pelgray <pelag.bv@gmail.com>
2020-09-07 15:04:49 +0300

    #6 Настроено подключение к Google таблице. При отправке боту сообщения с URL вакансии, весь текст сообщения будет добавлен в существующую, указанную в параметрах, Google таблицу.

--- src/main/java/com/pelgray/commands/AddVacancyURL.java
+++ src/main/java/com/pelgray/commands/AddVacancyURL.java
@@ -1,20 +1,34 @@
 package com.pelgray.commands;
 
+import com.pelgray.GoogleSheetsUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
 
 @Component
 public class AddVacancyURL implements ICommandHandler {
     private final static Logger LOG = LoggerFactory.getLogger(AddVacancyURL.class);
     protected final String DOMAIN = "hh.ru";
+    @Value("${gSheets.SpreadsheetId}")
+    private String spreadsheetId;
 
     @Override
     public SendMessage handle(Message msg) {
+        SendMessage sm = new SendMessage(msg.getChatId(), "Добавлено").setReplyToMessageId(msg.getMessageId());
+        try {
+            GoogleSheetsUtil.addTxtOnNewLine(msg.getText(), spreadsheetId);
+        } catch (IOException | GeneralSecurityException ex) {
+            LOG.error("Неудачная попытка добавления вакансии для пользователя " + msg.getFrom().getUserName(), ex);
+            return sm.setText("Не добавлено");
+        }
         LOG.info("Добавлена вакансия для пользователя {}", msg.getFrom().getUserName());
-        return new SendMessage(msg.getChatId(), "Добавлено").setReplyToMessageId(msg.getMessageId());
+        return sm;
     }
 
     @Override
@@ -23,6 +37,6 @@ public class AddVacancyURL implements ICommandHandler {
     }
 
     protected String regex() {
-        return "^https?:\\/\\/\\Q" + DOMAIN + "\\E\\/vacancy\\/\\d+(\\?.+)*$";
+        return "^https?:\\/\\/(?:\\w+\\.)?\\Q" + DOMAIN + "\\E\\/vacancy\\/\\d+(\\?.+)*$";
     }
 }
